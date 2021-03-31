3023035c1072014c74874fa267cc7e308f17d5a4
tianhaopeng <tianhaopeng@bytedance.com>
2021-03-31 08:35:00 +0800

    update

---
+++ src/Q82.cpp
@@ -0,0 +1,42 @@
+//
+// Created by bytedance on 2021/3/26.
+//
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        auto *pt = head, *prev = head;
+        bool dul = false;
+        while (pt && pt->next) {
+            if (pt->next->val == pt->val) {
+                pt->next = pt->next->next;
+                dul = true;
+            } else {
+                if (dul) {
+                    if (pt == head) {
+                        head = head->next;
+                        pt = pt->next;
+                        prev = head;
+                    } else {
+                        prev->next=pt->next;
+                    }
+                }
+                dul= false;
+                prev = pt;
+                pt = pt->next;
+            }
+        }
+        return head;
+    }
+};
