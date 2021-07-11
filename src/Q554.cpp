9dbe284b493690b87466b3d457e0b630627f51e8
nero <tianhp00@hotmail.com>
2021-07-11 13:10:06 +0800

    Q554

--- src/Q554.cpp
+++ src/Q554.cpp
@@ -1,4 +1,27 @@
-//
-// Created by 田豪鹏 on 2021/5/2.
-//
+#include <vector>
+#include <unordered_map>
+#include <set>
 
+using namespace std;
+
+class Solution {
+public:
+    int leastBricks(vector<vector<int>> &wall) {
+        unordered_map<int, int> wall_map;
+        if (wall.empty()) {
+            return 0;
+        }
+        for (auto &row : wall) {
+            int loc = 0;
+            for (int j = 0; j < row.size() - 1; ++j) {
+                loc += row[j];
+                wall_map[loc] += 1;
+            }
+        }
+        int res = 0;
+        for (auto &[_, count]:wall_map) {
+            res = max(res, count);
+        }
+        return wall.size() - res;
+    }
+};
